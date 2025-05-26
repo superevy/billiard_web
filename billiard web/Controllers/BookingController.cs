44ba46c7959252ab536f36e398be1f3ff2823468
superevy <rennobagas@gmail.com>
2025-05-26 20:48:57 +0700

    first commit

---
+++ billiard web/Controllers/BookingController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using billiard_web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace billiard_web.Controllers
+{
+    public class BookingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Booking", new BookingModel()); 
+        }
+
+
+        [HttpPost]
+        public ActionResult Order(BookingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Order"] = model;
+                return RedirectToAction("ViewOrder");
+            }
+            return View("Index", model);
+        }
+
+        public ActionResult ViewOrder()
+        {
+            var model = TempData["Order"] as BookingModel;
+            return View(model);
+        }
+    }
+}
---
+++ billiard web/Models/BookingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace billiard_web.Models
+{
+    public class BookingModel
+    {
+        public string Meja { get; set; }
+        public TimeSpan Jam { get; set; }
+        public int LamaSewa { get; set; }
+        public int HargaPerJam => 30000;
+
+        public int TotalHarga => HargaPerJam * LamaSewa;
+    }
+}
